d926b91686b2beb0a899921684aba519404b7764
felix-richard <105433778+felix-richard@users.noreply.github.com>
2023-02-13 17:40:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,42 @@ function setup()
 {
 canvas = createCanvas(280,280);
 canvas.center();
-background("#0a0908")
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechSynthesis;
 }
 function clearCanvas()
 {
-    background("#0a0908")
+    background("white");
 }
+function preload()
+{
+    classifier = ml5.imageClassifier("DoodleNet");    
+}
+function draw()
+{
+    strokeWeight(13);
+    stroke(0,0,255);
+    if (mouseIsPressed)
+    {
+        line(pmouseX,pmouseY,mouseX,mouseY); 
+    }
+}
+
+function classifyCanvas()
+    {
+        classifier.classify(canvas,gotResult);
+    }
+
+    function gotResult(error,results)
+    {
+        if (error){
+            console.error(error);
+        }
+        console.log(results);
+        document.getElementById('label').innerHTML = '  Label: '+results[0].label;
+
+        document.getElementById("confidence").innerHTML = 'confidence: ' + Math.round(results[0].confidence * 100)+ "%";
+        utterThis = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
